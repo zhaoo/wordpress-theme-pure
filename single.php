b775e91d8e7ba847e6a1149ee5c8d907d30f452a
zhaoo <894519210@qq.com>
2018-09-29 19:49:58 +0800

    v1.0.0
    Add article info.

--- single.php
+++ single.php
@@ -2,6 +2,11 @@
 <div class="container">
     <article class="article">
         <h3 class="article-title"><?php the_title(); ?></h3>
+        <div class="article-info">
+            <span><i class="fa fa-clock-o"></i><?php the_time('Y-n-j'); ?></span>
+            <span><i class="fa fa-eye"></i>255</span>
+            <span><i class="fa fa-file-text"></i><?php edit_post_link('编辑'); ?></span>
+        </div>
         <div class="content">
             <?php while (have_posts()): the_post(); the_content(); endwhile; ?>
         </div>
